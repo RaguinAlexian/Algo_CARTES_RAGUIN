5fa1da62ef3642bb697fcf2b0271776996b720e2
RaguinAlexian <a.raguin@rns.ecolescreatives.com>
2022-05-06 10:23:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ class Mage:
     def getCurrentMana(self) :
         return self.__currentMana
 
-    def getCard(self) :
-        self.__hand.append(self.__deck.pop())
 
     def gainMana(self) :
         self.__currentMana = self.__manaMax
@@ -36,6 +34,9 @@ class Mage:
     def changeHealth(self, change) :
         self.__health = self.__health + change
 
+    def drawCard(self) :
+        self.__hand.append(self.__deck.pop())
+
     def playCard(self, choose) :
         if(self.__availableCase < 5) :
             self.__gameCard.append(choose)
@@ -44,17 +45,20 @@ class Mage:
         else :
             return "Impossible"
 
-    def showHand(self) :
-        for i in range(len(self.__hand)) :
-            return self.__hand[i]
+    def showHand(self, choose) :
+        return self.__hand[choose]
 
-    def showCimetery(self) :
-        for i in range(len(self.__discard)) :
-            return self.__discard[i]
+    def showCimetery(self, choose) :
+        return self.__discard[choose]
 
-    def showPlayed(self) :
-        for i in range(len(self.__gameCard)) :
-            return self.__gameCard[i]
+    def showPlayed(self, choose) :
+        return self.__gameCard[choose]
+
+    def deckMaking(self, card) :
+        self.__deck.append(card)
+
+    def deckShuffle(self):        
+        random.shuffle(self.__deck)
 
 class Carte(Mage):
     def __init__(self, coutMana, nom, desc):
@@ -68,7 +72,7 @@ class Carte(Mage):
     def getName(self) :
         return self.__name
 
-    def getDescrition(self) :
+    def getDescription(self) :
         return self.__description
 
     def goToCimetery(self, choose) :
@@ -77,24 +81,31 @@ class Carte(Mage):
         self.__availableCase = self.__availableCase - 1
 
 class Blast(Carte):
-    def __init__(self, dmg):
+    def __init__(self, cost, name, desc, dmg):
+        Carte.__init__(self, cost, name, desc)
         self.__damage = dmg
 
     def damageBlast(self) :
         return self.__damage
 
 class Creature(Carte):
-    def __init__(self, pv, charge):
+    def __init__(self, valeur, name, desc, pv, charge):
+        Carte.__init__(self, valeur, name, desc)
         self.__lifePoint = pv
         self.__atk = charge
-        self.__hasCharged = False
+        self.__hasCharged = False        
 
     def getAtk(self) :
         if(self.__hasCharged == True) :
             return "impossible"
         else :
-            self.__hasCharged = True
             return self.__atk
+
+    def hasPlayed(self) :
+        if(self.__hasCharged == False) :
+            self.__hasCharged = True
+        else :
+            self.__hasCharged = False
 
     def pvChange(self, change) :
         self.__lifePoint = self.__lifePoint + change
@@ -104,7 +115,8 @@ class Creature(Carte):
 
 
 class Cristal(Carte):
-    def __init__(self, manaMaxPlus):
+    def __init__(self, manaCost, name, desc, manaMaxPlus):
+        Carte.__init__(self, manaCost, name, desc)
         self.__value = manaMaxPlus
 
     def changeManaMax(self) :
@@ -113,7 +125,25 @@ class Cristal(Carte):
     def getValue(self) :
         return self.__value
 
-class Shuffle(Carte):
+joueur1 = Mage("Benoit",25,10)
+joueur2 = Mage("Albert",25,10)
 
-    def deckShuffle(self):        
-        random.shuffle(self.__deck)
+card1 = Cristal(6, "Cristal de terre", "Ce cristal se plante dans la terre avant de redistribuer son énergie, il rapporte 3 mana supplémentaire", 3)
+card2 = Blast(2, "Vague de glace", "Une explosion de glace vient frapper l'adversaire", 4)
+card3 = Creature(1, "Slime nul", "Ce n'est qu'un pauvre slime", 1, 1)
+
+for i in range(3) :
+    joueur1.deckMaking(card1)
+    joueur1.deckMaking(card2)
+    joueur1.deckMaking(card3)
+
+joueur1.deckShuffle()
+joueur1.drawCard()
+print(joueur1.showHand(0).getName())
+print("La carte coûte",joueur1.showHand(0).getMana())
+print("Description :",joueur1.showHand(0).getDescription())
+if(isinstance(joueur1.showHand(0),Creature)) :
+    print("Attaque", joueur1.showHand(0).getAtk())
+    print("Pv", joueur1.showHand(0).getPv())
+if(isinstance(joueur1.showHand(0),Blast)) :
+    print("Inflige",joueur1.showHand(0).damageBlast())
